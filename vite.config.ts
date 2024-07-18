39d3b9b353662d5a56b0d9cf229622257434c7c3
amarachiugwu <marycynthiamara2@gmail.com>
2024-07-18 15:18:48 +0100

    dynamic import build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import dynamicImport from 'vite-plugin-dynamic-import'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    dynamicImport()
-  ],
-  
-  assetsInclude: ['**/*.md'],
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, 'src'),
+    plugins: [
+        react()
+    ],
+    assetsInclude: ['**/*.md'],
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'src'),
+        },
     },
-  },
-  build: {
-    outDir: 'build'
-  }
-})
+    build: {
+        outDir: 'build'
+    }
+});
