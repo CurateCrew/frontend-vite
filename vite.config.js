df4e4d1be9a74b7ea6eee6e8ca31447d042e538c
amarachiugwu <marycynthiamara2@gmail.com>
2024-07-18 15:04:54 +0100

    dynamic rendering build fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import dynamicImport from 'vite-plugin-dynamic-import';
-// https://vitejs.dev/config/
+
 export default defineConfig({
-    plugins: [react(),
-        dynamicImport()
+    plugins: [
+        react()
     ],
     assetsInclude: ['**/*.md'],
     resolve: {
